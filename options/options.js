718de0a677744f7a6f9a6de818287a5a3e68c206
Crowdbotics <team@crowdbotics.com>
2023-09-14 03:58:26 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "clubcrush-43377",
+  url: "https://clubcrush-43377.botics.co",
+  api: "https://clubcrush-43377.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
